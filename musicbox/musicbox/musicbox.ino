7166fe494b58d5d0c54178eb962684ae2e484bc2
Kevin Herr <kherr9@gmail.com>
2016-11-25 11:09:23 -0600

    setup led

--- musicbox/musicbox/musicbox.ino
+++ musicbox/musicbox/musicbox.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
 
+const int RED_LED_PIN = 13;
+
+void setup() {
+  pinMode(RED_LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(RED_LED_PIN, HIGH);
 
+  delay(1000);
+
+  digitalWrite(RED_LED_PIN, LOW);
+
+  delay(1000);
 }
