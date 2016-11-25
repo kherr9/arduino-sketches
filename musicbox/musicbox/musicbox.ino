95833b9a43808e688a762e95dae829b34b063f19
Kevin Herr <kherr9@gmail.com>
2016-11-25 11:42:15 -0600

    function pointers

--- musicbox/musicbox/musicbox.ino
+++ musicbox/musicbox/musicbox.ino
@@ -3,9 +3,19 @@ const int RED_LED_PIN = 13;
 const int BTN_PIN = 2;
 int btnState = 0;
 
+// declare type for button handler
+// arg[0] - button pin
+// arg[1] - button state
+typedef void (*ButtonHandler)(int, int);
+
+// declare array
+ButtonHandler buttonHandlers[1];
+
 void setup() {
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(BTN_PIN, INPUT);
+
+  buttonHandlers[0] = &updateLed;
 }
 
 void loop() {
@@ -15,22 +25,18 @@ void loop() {
   if(tempBtnState != btnState){
     // button state has changed
 
-    // delcare function pointer
-    void (*functionPtr)(int);
-
-    // assign function pointer
-    functionPtr = &updateLed;
-
-    // invoke function pointer
-    (*functionPtr)(btnState);
+    for(int i = 0; i < 1; i++){
+      ButtonHandler handler = buttonHandlers[i];
+      handler(BTN_PIN, tempBtnState);
+    }
 
     // update button state
     btnState = tempBtnState;
   }
 }
 
-void updateLed(int state){
-  if(state == LOW){
+void updateLed(int btnPin, int state){
+  if(state == HIGH){
     digitalWrite(RED_LED_PIN, HIGH);  
   }else{
     digitalWrite(RED_LED_PIN, LOW);
