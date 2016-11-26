0e72298039ec18751be1ad23d137fc4d00f89e3e
Kevin Herr <kherr9@gmail.com>
2016-11-26 09:44:45 -0600

    stub music constructs

--- musicbox/musicbox/musicbox.ino
+++ musicbox/musicbox/musicbox.ino
@@ -5,6 +5,56 @@ const int GREEN_LED_PIN = 12;
 
 Button button(2);
 
+enum NoteLength {
+  Eigth,
+  Quarter,
+  Half,
+  Whole
+};
+
+enum Notes {
+  A,B,C,D,E,F,G  
+};
+
+struct Note {
+  NoteLength length;
+  Notes note;
+  
+  Note(Notes note, NoteLength length){
+    this->note = note;
+    this->length = length;
+  }
+};
+
+Note marryHadALittleLamb[] = {
+  {B, Quarter},
+  {A, Quarter},
+  {G, Quarter},
+  {A, Quarter},
+  {B, Quarter},
+  {B, Quarter},
+  {B, Quarter},
+  {A, Quarter},
+  {A, Quarter},
+  {A, Quarter},
+  {B, Quarter},
+  {D, Quarter},
+  {D, Quarter},
+  {B, Quarter},
+  {A, Quarter},
+  {G, Quarter},
+  {A, Quarter},
+  {B, Quarter},
+  {B, Quarter},
+  {B, Quarter},
+  {B, Quarter},
+  {A, Quarter},
+  {A, Quarter},
+  {B, Quarter},
+  {A, Quarter},
+  {G, Quarter}
+};
+
 void setup() {
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
