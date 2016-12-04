f4ef0e8f55f491d2093da2ee77ecdd757bc87419
Kevin Herr <kherr9@gmail.com>
2016-12-04 12:29:21 -0600

    sync bluetooth

--- musicbox/wii_blinking_light/wii_blinking_light/wii_blinking_light.ino
+++ musicbox/wii_blinking_light/wii_blinking_light/wii_blinking_light.ino
@@ -1,56 +1,112 @@
-/*#include <address.h>
-#include <adk.h>
-#include <avrpins.h>
-#include <BTD.h>
-#include <BTHID.h>
-#include <cdcacm.h>
-#include <cdcftdi.h>
-#include <cdcprolific.h>
-#include <cdc_XR21B1411.h>
-#include <confdescparser.h>
-#include <controllerEnums.h>
-#include <hexdump.h>
-#include <hid.h>
-#include <hidboot.h>
-#include <hidescriptorparser.h>
-#include <hiduniversal.h>
-#include <hidusagestr.h>
-#include <macros.h>
-#include <masstorage.h>
-#include <max3421e.h>
-#include <max_LCD.h>
-#include <message.h>
-#include <parsetools.h>
-#include <printhex.h>
-#include <PS3BT.h>
-#include <PS3Enums.h>
-#include <PS3USB.h>
-#include <PS4BT.h>
-#include <PS4Parser.h>
-#include <PS4USB.h>
-#include <PSBuzz.h>
-#include <settings.h>
-#include <sink_parser.h>
-#include <SPP.h>*/
-#include <Usb.h>
-#include <UsbCore.h>/*
-#include <usbhost.h>
 #include <usbhub.h>
-#include <usb_ch9.h>
-#include <version_helper.h>
 #include <Wii.h>
-#include <xboxEnums.h>
-#include <XBOXOLD.h>
-#include <XBOXONE.h>
-#include <XBOXRECV.h>
-#include <XBOXUSB.h>
-*/
+
+#define ENABLE_UHS_DEBUGGING 1
+#define EXTRADEBUG 1
+#define PRINTREPORT 1
+
+USB Usb;
+//USBHub Hub1(&Usb); // Some dongles have a hub inside
+
+BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
+/* You can create the instance of the class in two ways */
+//WII Wii(&Btd, PAIR); // This will start an inquiry and then pair with your Wiimote - you only have to do this once
+WII Wii(&Btd); // After that you can simply create the instance like so and then press any button on the Wiimote
+
+bool printAngle;
+
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+#if !defined(__MIPSEL__)
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+  if (Usb.Init() == -1) {
+    Serial.print(F("\r\nOSC did not start"));
+    while (1); //halt
+  }
+  Serial.print(F("\r\nWiimote Bluetooth Library Started"));
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  Usb.Task();
+  if (Wii.wiimoteConnected) {
+    if (Wii.getButtonClick(HOME)) { // You can use getButtonPress to see if the button is held down
+      Serial.print(F("\r\nHOME"));
+      Wii.disconnect();
+    }
+    else {
+      if (Wii.getButtonClick(LEFT)) {
+        Wii.setLedOff();
+        Wii.setLedOn(LED1);
+        Serial.print(F("\r\nLeft"));
+      }
+      if (Wii.getButtonClick(RIGHT)) {
+        Wii.setLedOff();
+        Wii.setLedOn(LED3);
+        Serial.print(F("\r\nRight"));
+      }
+      if (Wii.getButtonClick(DOWN)) {
+        Wii.setLedOff();
+        Wii.setLedOn(LED4);
+        Serial.print(F("\r\nDown"));
+      }
+      if (Wii.getButtonClick(UP)) {
+        Wii.setLedOff();
+        Wii.setLedOn(LED2);
+        Serial.print(F("\r\nUp"));
+      }
 
+      if (Wii.getButtonClick(PLUS)){
+        Serial.print(F("\r\nPlus"));
+        Serial.println(Btd.GetAddress());
+      }
+      if (Wii.getButtonClick(MINUS))
+        Serial.print(F("\r\nMinus"));
+
+      if (Wii.getButtonClick(ONE))
+        Serial.print(F("\r\nOne"));
+      if (Wii.getButtonClick(TWO))
+        Serial.print(F("\r\nTwo"));
+
+      if (Wii.getButtonClick(A)) {
+        printAngle = !printAngle;
+        Serial.print(F("\r\nA"));
+      }
+      if (Wii.getButtonClick(B)) {
+        Wii.setRumbleToggle();
+        Serial.print(F("\r\nB"));
+      }
+    }
+#if 0 // Set this to 1 in order to see the angle of the controllers
+    if (printAngle) {
+      Serial.print(F("\r\nPitch: "));
+      Serial.print(Wii.getPitch());
+      Serial.print(F("\tRoll: "));
+      Serial.print(Wii.getRoll());
+      if (Wii.motionPlusConnected) {
+        Serial.print(F("\tYaw: "));
+        Serial.print(Wii.getYaw());
+      }
+      if (Wii.nunchuckConnected) {
+        Serial.print(F("\tNunchuck Pitch: "));
+        Serial.print(Wii.getNunchuckPitch());
+        Serial.print(F("\tNunchuck Roll: "));
+        Serial.print(Wii.getNunchuckRoll());
+      }
+    }
+#endif
+  }
+#if 0 // Set this to 1 if you are using a Nunchuck controller
+  if (Wii.nunchuckConnected) {
+    if (Wii.getButtonClick(Z))
+      Serial.print(F("\r\nZ"));
+    if (Wii.getButtonClick(C))
+      Serial.print(F("\r\nC"));
+    if (Wii.getAnalogHat(HatX) > 137 ||  Wii.getAnalogHat(HatX) < 117 || Wii.getAnalogHat(HatY) > 137 || Wii.getAnalogHat(HatY) < 117) {
+      Serial.print(F("\r\nHatX: "));
+      Serial.print(Wii.getAnalogHat(HatX));
+      Serial.print(F("\tHatY: "));
+      Serial.print(Wii.getAnalogHat(HatY));
+    }
+  }
+#endif
 }
