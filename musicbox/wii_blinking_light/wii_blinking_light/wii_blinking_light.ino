177774e32220ce24551075d7afed95b6c8db1d35
Kevin Herr <kherr9@gmail.com>
2016-12-04 11:13:55 -0600

    import lib

--- musicbox/wii_blinking_light/wii_blinking_light/wii_blinking_light.ino
+++ musicbox/wii_blinking_light/wii_blinking_light/wii_blinking_light.ino
@@ -1,3 +1,50 @@
+/*#include <address.h>
+#include <adk.h>
+#include <avrpins.h>
+#include <BTD.h>
+#include <BTHID.h>
+#include <cdcacm.h>
+#include <cdcftdi.h>
+#include <cdcprolific.h>
+#include <cdc_XR21B1411.h>
+#include <confdescparser.h>
+#include <controllerEnums.h>
+#include <hexdump.h>
+#include <hid.h>
+#include <hidboot.h>
+#include <hidescriptorparser.h>
+#include <hiduniversal.h>
+#include <hidusagestr.h>
+#include <macros.h>
+#include <masstorage.h>
+#include <max3421e.h>
+#include <max_LCD.h>
+#include <message.h>
+#include <parsetools.h>
+#include <printhex.h>
+#include <PS3BT.h>
+#include <PS3Enums.h>
+#include <PS3USB.h>
+#include <PS4BT.h>
+#include <PS4Parser.h>
+#include <PS4USB.h>
+#include <PSBuzz.h>
+#include <settings.h>
+#include <sink_parser.h>
+#include <SPP.h>*/
+#include <Usb.h>
+#include <UsbCore.h>/*
+#include <usbhost.h>
+#include <usbhub.h>
+#include <usb_ch9.h>
+#include <version_helper.h>
+#include <Wii.h>
+#include <xboxEnums.h>
+#include <XBOXOLD.h>
+#include <XBOXONE.h>
+#include <XBOXRECV.h>
+#include <XBOXUSB.h>
+*/
 void setup() {
   // put your setup code here, to run once:
 
