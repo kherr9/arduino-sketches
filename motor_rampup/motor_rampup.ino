ce7eb680283d50d03afb14595d988cee13d664c9
Kevin Herr <kherr9@gmail.com>
2016-12-17 18:48:42 -0600

    commit

---
+++ motor_rampup/motor_rampup.ino
@@ -0,0 +1,53 @@
+#include <SparkFun_TB6612.h>
+
+// Basic sketch for trying out the Adafruit DRV8871 Breakout
+
+#define AIN1 2
+#define AIN2 4
+#define PWMA 5
+#define STBY 9
+
+#define BIN1 3
+#define BIN2 8
+#define PWMB 6
+
+
+// these constants are used to allow you to make your motor configuration 
+// line up with function names like forward.  Value can be 1 or -1
+const int offsetA = 1;
+const int offsetB = 1;
+
+// Initializing motors.  The library will allow you to initialize as many
+// motors as you have memory for.  If you are using functions like forward
+// that take 2 motors as arguements you can either write new functions or
+// call the function more than once.
+////Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+////Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+
+Motor motor1 = Motor(2, 4, 3, offsetA, STBY);
+Motor motor2 = Motor(5, 10, 6, offsetA, STBY);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DRV8871 test");
+
+  delay(2000);
+}
+
+void loop() {
+  
+  int rpm = 150;//255;
+  int sleep = 2000;
+   
+  motor1.drive(rpm);
+  //motor2.drive(rpm);
+  delay(sleep);
+
+  motor1.drive(rpm * -1);
+  //motor2.drive(rpm * -1);
+  delay(sleep);
+
+  motor1.brake();
+  //motor2.brake();
+  delay(sleep);
+}
